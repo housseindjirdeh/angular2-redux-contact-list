f865e4c73b03235f6155a476bbf04b0e3e40e669
hdjirdeh <h.djirdeh@mail.utoronto.ca>
2016-07-03 13:54:07 -0400

    remove js rendered files

--- app/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var contact_list_1 = require('./contact-list');
-var contact_store_1 = require('./contact-store');
-platform_browser_dynamic_1.bootstrap(contact_list_1.ContactList, [contact_store_1.ContactStore]);
-//# sourceMappingURL=main.js.map
